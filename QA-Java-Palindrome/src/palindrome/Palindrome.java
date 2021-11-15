4042c44f3d9c6144f2858f47474a581080974f77
Ekin <64873688+eo-uk@users.noreply.github.com>
2021-11-15 10:53:17 +0000

    fixed indentation

--- QA-Java-Palindrome/src/palindrome/Palindrome.java
+++ QA-Java-Palindrome/src/palindrome/Palindrome.java
@@ -2,22 +2,23 @@ package palindrome;
 
 public class Palindrome {
 	public static boolean isPalindrome(String s) {
-        for (int i=s.length()-1, j=0; i>=0 && j<s.length(); i--, j++) {
-        	if (s.charAt(i) != s.charAt(j)) {
-        		return false;
+        	for (int i=s.length()-1, j=0; i>=0 && j<s.length(); i--, j++) {
+        		if (s.charAt(i) != s.charAt(j)) {
+        			return false;
+        		}
+            		if (i == j) {
+            			break; // break halfway for better performance
+            		}
         	}
-            if (i == j) {
-            	break; // break halfway for better performance
-            }
-        }
-        return true;
-    }
+        	return true;
+    	}
 	
 	public static boolean listAll(String[] words) {
 		System.out.println("Palindrome List\n-----------------------");
 		for (String word : words) {
 			System.out.println(
-					word + (isPalindrome(word) ? " is a palindrome" : " is not a palindrome"));
+					word + (isPalindrome(word) ? " is a palindrome" : " is not a palindrome")
+			);
 		}
 		return true;
 	}
