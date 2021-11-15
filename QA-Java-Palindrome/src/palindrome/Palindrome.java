1ebd4307f9604e27ce4566d178e28d67151a3d92
eo-uk <london.ekin@gmail.com>
2021-11-15 10:50:27 +0000

    added project

---
+++ QA-Java-Palindrome/src/palindrome/Palindrome.java
@@ -0,0 +1,24 @@
+package palindrome;
+
+public class Palindrome {
+	public static boolean isPalindrome(String s) {
+        for (int i=s.length()-1, j=0; i>=0 && j<s.length(); i--, j++) {
+        	if (s.charAt(i) != s.charAt(j)) {
+        		return false;
+        	}
+            if (i == j) {
+            	break; // break halfway for better performance
+            }
+        }
+        return true;
+    }
+	
+	public static boolean listAll(String[] words) {
+		System.out.println("Palindrome List\n-----------------------");
+		for (String word : words) {
+			System.out.println(
+					word + (isPalindrome(word) ? " is a palindrome" : " is not a palindrome"));
+		}
+		return true;
+	}
+}
---
+++ QA-Java-Palindrome/src/palindrome/Runner.java
@@ -0,0 +1,8 @@
+package palindrome;
+
+public class Runner {
+	public static void main(String[] args) {
+		String[] words = {"level", "tree", "house", "radar"};
+		Palindrome.listAll(words);
+	}
+}
